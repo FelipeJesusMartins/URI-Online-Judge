14b25a14f5ad9c0a0a965af7a47fe6883000d9b6
FelipeJesusMartins <felipemartins1359@gmail.com>
2020-08-04 00:45:50 -0300

    1067

---
+++ Iniciante/1071 Soma de Impares Consecutivos I URI/1071 Soma de Impares Consecutivos I URI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1071_Soma_de_Impares_Consecutivos_I_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, max, min, soma;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                max = x;
+                min = y;
+            }
+            else
+            {
+                max = y;
+                min = x;
+            }
+
+            soma = 0;
+            for (int i = min + 1; i < max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine(soma);
+            Console.ReadKey();
+        }
+    }
+}
