482d42766a97b77aac0addd4341d82d72a7a0a0b
FelipeJesusMartins <felipemartins1359@gmail.com>
2020-08-05 10:34:35 -0300

    1133 - 1157

---
+++ Iniciante/1153 Fatorial Simples URI/1153 Fatorial Simples URI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1153_Fatorial_Simples_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long fat = 1;
+            for (int i = 1; i <= n; i++) {
+                fat = fat * i;
+            }
+
+            Console.WriteLine(fat);
+            Console.ReadKey();
+        }
+    }
+}
