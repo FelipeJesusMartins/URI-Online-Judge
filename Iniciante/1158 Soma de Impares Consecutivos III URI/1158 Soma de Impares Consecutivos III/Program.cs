3a87f21693ab08bd08dacf5733995a77c9e259c4
FelipeJesusMartins <felipemartins1359@gmail.com>
2020-08-10 14:54:53 -0300

    1099 - 1165

---
+++ Iniciante/1158 Soma de Impares Consecutivos III URI/1158 Soma de Impares Consecutivos III/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1158_Soma_de_Impares_Consecutivos_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                int x = int.Parse(valores[0]);
+                int y = int.Parse(valores[1]);
+                int soma = 0;
+
+                for (int p = 0; p < y;x++)
+                {
+                    if (x % 2 != 0)
+                    {
+                        soma += x;
+                        p++;
+                    }
+                }
+                Console.WriteLine(soma);
+            }
+            Console.ReadKey();
+        }
+    }
+}
