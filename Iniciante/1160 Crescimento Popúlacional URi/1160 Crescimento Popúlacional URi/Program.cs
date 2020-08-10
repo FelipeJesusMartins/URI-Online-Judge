1857a02ecdc10fe5e40e47be4d1923fde371997f
FelipeJesusMartins <felipemartins1359@gmail.com>
2020-08-10 14:49:02 -0300

    1099 - 1165

---
+++ Iniciante/1160 Crescimento Popúlacional URi/1160 Crescimento Popúlacional URi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1160_Crescimento_Popúlacional_URi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
