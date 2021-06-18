5b571f0fde5c408832d675b0ba553c1ae3e61b50
FelipeJesusMartins <felipemartins1359@gmail.com>
2021-06-18 10:42:21 -0300

    URI 1164 completed

---
+++ CSharp/1164 Numero Perfeito URI/1164 Numero Perfeito URI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1164_Numero_Perfeito_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialValue = int.Parse(Console.ReadLine());
+
+            if (initialValue >= 1 && initialValue <= 20)
+            {
+                for (int counter = 0; counter < initialValue; counter++)
+                {
+                    int finalValue = 0;
+                    int y = 0;
+
+                    int x = int.Parse(Console.ReadLine());
+
+                    for (y = 1; y < x; y++)
+                    {
+                        if (x % y == 0)
+                        {
+                            finalValue += y;
+                        }
+                    }
+                    if (finalValue == x)
+                    {
+                        Console.WriteLine($"{x} eh perfeito");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{x} nao eh perfeito");
+                    }
+                }
+            }
+            else
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
