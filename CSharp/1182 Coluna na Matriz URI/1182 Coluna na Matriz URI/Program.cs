add82f658b33a337a31e1224eb18d32673e57a3a
FelipeJesusMartins <felipemartins1359@gmail.com>
2021-07-21 14:27:07 -0300

    1182 completed

---
+++ CSharp/1182 Coluna na Matriz URI/1182 Coluna na Matriz URI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1182_Coluna_na_Matriz_URI
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            int arrColumn = int.Parse(Console.ReadLine());
+            string op = Console.ReadLine();
+            double[,] arr = new double[12, 12];
+            double sum = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    arr[i, j] = double.Parse(Console.ReadLine());
+                    if (j == arrColumn)
+                    {
+                        sum += arr[i, j];
+                    }
+                }
+            }
+            if (op.Equals("S"))
+            {
+                string final = string.Format("{0:0.0}", sum);
+                final = final.Replace(",", ".");
+                Console.WriteLine(final);
+            }
+            else if (op.Equals("M"))
+            {
+                double r = sum / 12.0;
+                string final = string.Format("{0:0.0}", r);
+                final = final.Replace(",", ".");
+                Console.WriteLine(final);
+            }
+        }
+    }
+}
