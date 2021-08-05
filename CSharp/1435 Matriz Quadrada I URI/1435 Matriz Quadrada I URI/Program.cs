f230bd138b8a53fd0a77da7e9d43ba0060ae29f1
FelipeJesusMartins <felipemartins1359@gmail.com>
2021-08-05 00:40:23 -0300

    1435 started

---
+++ CSharp/1435 Matriz Quadrada I URI/1435 Matriz Quadrada I URI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1435_Matriz_Quadrada_I_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            string final = "";
+
+            do
+            {
+                b = int.Parse(Console.ReadLine());
+                int[,] arr = new int[b, b];
+                for (int i = 0; i < b; i++)
+                {
+                    for (int j = 0; j < b; j++)
+                    {
+                        arr[i, j] = b;
+                    }
+                }
+                for (int i = 0; i < b; i++)
+                {
+                    for (int j = 0; j < b; j++)
+                    {
+                        final += $" {arr[i, j]}";
+                    }
+                    final += "\n";
+                }
+                Console.WriteLine(final);
+                final = string.Empty;
+            }
+
+            while (b > 0);
+        }
+
+    }
+}
