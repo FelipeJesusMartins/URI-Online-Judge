21dc21b6c5a7234084f192108210b7fe0b8f875a
FelipeJesusMartins <felipemartins1359@gmail.com>
2021-10-13 15:24:28 -0300

    started kotlin

--- CSharp/1435 Matriz Quadrada I URI/1435 Matriz Quadrada I URI/Program.cs
+++ CSharp/1435 Matriz Quadrada I URI/1435 Matriz Quadrada I URI/Program.cs
@@ -6,34 +6,67 @@ namespace _1435_Matriz_Quadrada_I_URI
     {
         static void Main(string[] args)
         {
-            int b;
-            string final = "";
+            int N = int.Parse(Console.ReadLine());
+            //while(N == 1)
+            //{
+                int
+                    a,
+                    b,
+                    c,
+                    g,
+                    h,
+                    i,
+                    j = 1,
+                    p = 0,
+                    q = 0,
+                    r = 0;
 
-            do
-            {
-                b = int.Parse(Console.ReadLine());
-                int[,] arr = new int[b, b];
-                for (int i = 0; i < b; i++)
-                {
-                    for (int j = 0; j < b; j++)
-                    {
-                        arr[i, j] = b;
-                    }
-                }
-                for (int i = 0; i < b; i++)
-                {
-                    for (int j = 0; j < b; j++)
-                    {
-                        final += $" {arr[i, j]}";
-                    }
-                    final += "\n";
-                }
-                Console.WriteLine(final);
-                final = string.Empty;
-            }
 
-            while (b > 0);
+                if (N == 0) Environment.Exit(0);
+
+                int[,] arr = new int[N, N];
+
+                i = N;
+                if (N % 2 == 0)
+                {
+                    r = N / 2;
+                }
+                else if (N % 2 == 1)
+                {
+                    r = (N / 2) + 1;
+                }
+                for (c = 1; c <= r; c++)
+                {
+                    for (a = p; a < i; a++)
+                    {
+                        for (b = q; b < i; b++)
+                        {
+                            arr[a, b] = j;
+                        }
+                    }
+                    j++;
+                    p++;
+                    q++;
+                    i--;
+                }
+                for (g = 0; g < N; g++)
+                {
+                    for (h = 0; h < N; h++)
+                    {
+                        if (h == 0)
+                        {
+                            Console.Write($"{arr[g, h]}");
+                        }
+                        else
+                        {
+                            Console.Write($"{arr[g, h]}");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                }
+            //}
+            // N = int.Parse(Console.ReadLine());
         }
-
     }
 }
