adc40923f8d5093d5060ca2d42c9f822bcea3164
FelipeJesusMartins <felipemartins1359@gmail.com>
2021-12-14 17:27:51 -0300

    Simplified URI 1001

--- CSharp/1001 Extremamente basico URI/1001 Extremamente basico URI/Program.cs
+++ CSharp/1001 Extremamente basico URI/1001 Extremamente basico URI/Program.cs
@@ -6,16 +6,7 @@ namespace _1001_Extremamente_basico_URI
     {
         static void Main(string[] args)
         {
-            int a, b, x;
-
-            a = int.Parse(Console.ReadLine());
-            b = int.Parse(Console.ReadLine());
-
-            x = a + b;
-
-            Console.WriteLine("X = " + x);
-
-            Console.ReadKey();
+            Console.WriteLine($"X = {int.Parse(Console.ReadLine())+ int.Parse(Console.ReadLine())}");
         }
     }
 }
