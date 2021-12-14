16142e8358a3e781c23e16c035fd360bff014ba0
FelipeJesusMartins <felipemartins1359@gmail.com>
2021-12-14 17:38:27 -0300

    Simplified URI 1002

--- CSharp/1002 Area do circulo URI/1002 Area do circulo URI/Program.cs
+++ CSharp/1002 Area do circulo URI/1002 Area do circulo URI/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 namespace _1002_Area_do_circulo_URI
 {
@@ -7,9 +6,7 @@ namespace _1002_Area_do_circulo_URI
     {
         static void Main(string[] args)
         {
-            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine($"A={(3.14159 * (raio * raio)).ToString("F4", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"A={(3.14159 * Math.Pow(double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture), 2)).ToString("F4", System.Globalization.CultureInfo.InvariantCulture)}");
         }
     }
 }
