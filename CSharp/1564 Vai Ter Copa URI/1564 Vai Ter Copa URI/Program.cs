ba7a486d33b6bc96e2518defc6e13b7de2dd2ee3
FelipeJesusMartins <felipemartins1359@gmail.com>
2021-12-30 09:59:16 -0300

    1564 in CSharp

---
+++ CSharp/1564 Vai Ter Copa URI/1564 Vai Ter Copa URI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1564_Vai_Ter_Copa_URI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            while (!string.IsNullOrEmpty(x = Console.ReadLine()))
+            {
+                Console.WriteLine((int.Parse(x) == 0) ? "vai ter copa!" : "vai ter duas!");
+            }
+        }
+    }
+}
