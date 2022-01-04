7ed77a865b2ea129a7b0bfc83b5d9f297d221e11
FelipeJesusMartins <felipemartins1359@gmail.com>
2022-01-04 15:48:05 -0300

    2344 in C#

---
+++ CSharp/2344 Notas da Prova URI/2344 Notas da Prova URI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2344_Notas_da_Prova_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = sbyte.Parse(Console.ReadLine());
+            if (x > 0 && x < 36) { Console.WriteLine("D"); }
+            else if (x > 35 && x < 61) { Console.WriteLine("C"); }
+            else if (x > 60 && x < 86) { Console.WriteLine("B"); }
+            else if (x > 85 && x < 101) { Console.WriteLine("A"); }
+            else { Console.WriteLine("E"); }
+        }
+    }
+}
