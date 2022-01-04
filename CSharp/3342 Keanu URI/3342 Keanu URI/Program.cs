5ae826d8d1a0b50e0420579ce6eec181a2f914c5
FelipeJesusMartins <felipemartins1359@gmail.com>
2022-01-04 16:36:24 -0300

    3342 in C#

---
+++ CSharp/3342 Keanu URI/3342 Keanu URI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _3342_Keanu_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{(x * x) - ((x * x) / 2)} casas brancas e {(x * x) / 2} casas pretas");
+        }
+    }
+}
