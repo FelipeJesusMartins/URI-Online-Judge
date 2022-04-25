7c1f36ecaea66f42d6158a952adc56c3a231e0c4
Felipe de Jesus Martins <51520435+FelipeJesusMartins@users.noreply.github.com>
2022-04-25 10:40:47 -0300

    Updated 1036 in c

--- C/1036_Bhaskaras_Formula.c
+++ C/1036_Bhaskaras_Formula.c
@@ -3,15 +3,13 @@
 
 int main()
 {
-    double x, y, z, d;
+    double x, y, z;
 
     scanf("%lf %lf %lf", &x, &y, &z);
 
-    d = (y * y) - 4 * x * z;
-
-    if (x == 0 || d < 0.0)
+    if (x == 0 || ((y * y) - 4 * x * z) < 0.0)
         printf("Impossivel calcular\n");
     else
-        printf("R1 = %.5lf\nR2 = %.5lf\n", (-y + pow(d, 0.5)) / (2.0 * x), (-y - pow(d, 0.5)) / (2.0 * x));
+        printf("R1 = %.5lf\nR2 = %.5lf\n", (-y + pow(((y * y) - 4 * x * z), 0.5)) / (2.0 * x), (-y - pow(((y * y) - 4 * x * z), 0.5)) / (2.0 * x));
     return 0;
 }
