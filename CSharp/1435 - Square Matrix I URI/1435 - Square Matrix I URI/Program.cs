f0ffd309e4ad538abb8b8af8682f228824366445
Felipe Martins <felipemartins1359@gmail.com>
2024-03-22 15:12:51 -0300

    1435 in C#

---
+++ CSharp/1435 - Square Matrix I URI/1435 - Square Matrix I URI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int length;
+        do
+        {
+            length = int.Parse(Console.ReadLine());
+
+            if (length == 0)
+                break;
+
+            Creatematrix(length, 0);
+            Console.WriteLine();
+
+        } while (length != 0);
+    }
+
+    static void Creatematrix(int tamanho, int n)
+    {
+        if (n >= tamanho) return;
+        string val = "";
+        for (int j = 0; j < tamanho; j++)
+            val += $"{(Math.Min(Math.Min(n, j), Math.Min(tamanho - 1 - n, tamanho - 1 - j)) + 1),4}";
+
+        Console.WriteLine("  " + val.Trim());
+
+        Creatematrix(tamanho, n+1);
+    }
+}
