e4211bf5001a3d274ea30d51e4197f3e516833cc
Felipe Martins <felipemartins1359@gmail.com>
2024-03-22 15:25:26 -0300

    1049 in python

---
+++ Python/1049_Animal.py
@@ -0,0 +1,28 @@
+animal_map = {
+    "vertebrado": {
+        "ave": {
+            "carnivoro": "aguia",
+            "onivoro": "pomba"
+        },
+        "mamifero": {
+            "onivoro": "homem",
+            "herbivoro": "vaca"
+        }
+    },
+    "invertebrado": {
+        "inseto": {
+            "hematofago": "pulga",
+            "herbivoro": "lagarta"
+        },
+        "anelideo": {
+            "hematofago": "sanguessuga",
+            "onivoro": "minhoca"
+        }
+    }
+}
+
+in1 = input()
+in2 = input()
+in3 = input()
+
+print(animal_map[in1][in2][in3])
